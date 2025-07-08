fe99dd9d76d2aa7cbce3ea2e849f91b34b62d083
JukisYuri <yurichangirl752@gmail.com>
2025-07-08 23:02:56 +0700

    update tracklog

--- src/services/tracklog.js
+++ src/services/tracklog.js
@@ -5,22 +5,30 @@ async function trackLog(aiStatus, user, message, destinateChannelId, client) {
     try {
     const messageContent = message.content
     const destinateChannel = await client.channels.fetch(destinateChannelId)
+    const baseContext = `[${aiStatus}] ${user.username}: ${messageContent}`
+    let attachmentLinks = null
     if (destinateChannel.isTextBased()){
-        await takeLogToJson(aiStatus, user, messageContent)
-        await destinateChannel.send(`[${aiStatus}] ${user.username}: ${messageContent}`) 
+        if (message.attachments.size > 0) {
+            attachmentLinks = message.attachments.map(att => att.url).join('\n')
+            await destinateChannel.send(`${baseContext}\n${attachmentLinks}`)
+        } else {
+            await destinateChannel.send(`${baseContext}`) 
+        }
+        await takeLogToJson(aiStatus, user, messageContent, attachmentLinks)
         }
     } catch (err) {
         console.error(err)
     }
 }
 
-async function takeLogToJson(aiStatus, user, messageContent) {
+async function takeLogToJson(aiStatus, user, messageContent, attachmentLinks) {
     try {
     const logEntry = {
         status: aiStatus,
         user: user.username,
         timestamp: DateTime.now().setZone("Asia/Ho_Chi_Minh").toISO(),
-        message: messageContent
+        message: messageContent,
+        attachments: attachmentLinks
     }
     fs.appendFileSync('./src/database/trackinglog.jsonl', JSON.stringify(logEntry) + '\n')
     } catch (err){
